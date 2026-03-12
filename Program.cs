90cee7a1fac6a564f8ebdf097e24fcc97003a8d1
Pápai Roland <144841960+papairoland@users.noreply.github.com>
2026-03-12 18:59:50 +0100

    Dolgozó modell osztály definiálása
    Added a new class 'Dolgozo' with properties for employee details.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
-﻿namespace ConsoleApp2
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp2
 {
+    class Dolgozo
+{
+    public string Nev { get; set; }
+    public string Reszleg { get; set; }
+    public int Fizetes { get; set; }
+}
     internal class Program
     {
         static void Main(string[] args)
@@ -8,3 +18,4 @@
         }
     }
 }
+
