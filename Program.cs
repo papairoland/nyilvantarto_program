8a1777c5fa4f3f23e7e329beab8645578387b55e
Pápai Roland <144841960+papairoland@users.noreply.github.com>
2026-03-12 19:10:04 +0100

    Mentés metódus StreamWriter logikával
    Added a new method for saving employee data to a file.

--- Program.cs
+++ Program.cs
@@ -18,31 +18,38 @@ namespace ConsoleApp2
         {
             int valasztas = 0;
             while (valasztas != 5){
-            Console.Clear();
-            Console.WriteLine("=== DOLGOZÓI NYILVÁNTARTÓ RENDSZER ===");
-            Console.WriteLine("1. Listázás\n2. Felvétel\n3. Törlés\n4. Statisztika\n5. Kilépés");
-            Console.Write("Válasszon: ");
-            if (!int.TryParse(Console.ReadLine(), out valasztas)) continue;
+                Console.Clear();
+                Console.WriteLine("=== DOLGOZÓI NYILVÁNTARTÓ RENDSZER ===");
+                Console.WriteLine("1. Listázás\n2. Felvétel\n3. Törlés\n4. Statisztika\n5. Kilépés");
+                Console.Write("Válasszon: ");
+                if (!int.TryParse(Console.ReadLine(), out valasztas)) continue;
             }
 
             static void Felvetel(){
-            Console.WriteLine("\n--- ÚJ DOLGOZÓ ---");
-            Dolgozo uj = new Dolgozo();
-            Console.Write("Név: "); uj.Nev = Console.ReadLine();
-            Console.Write("Részleg: "); uj.Reszleg = Console.ReadLine();
-            Console.Write("Fizetés: "); uj.Fizetes = int.Parse(Console.ReadLine());
-            dolgozok.Add(uj);
+                Console.WriteLine("\n--- ÚJ DOLGOZÓ ---");
+                Dolgozo uj = new Dolgozo();
+                Console.Write("Név: "); uj.Nev = Console.ReadLine();
+                Console.Write("Részleg: "); uj.Reszleg = Console.ReadLine();
+                Console.Write("Fizetés: "); uj.Fizetes = int.Parse(Console.ReadLine());
+                dolgozok.Add(uj);
             }
 
             static void Listazas(){
-            if (dolgozok.Count == 0) Console.WriteLine("Üres az adatbázis.");
-            Console.WriteLine("\n--- LISTA ---");
-            for (int i = 0; i < dolgozok.Count; i++){
-            Console.WriteLine($"{i + 1}. {dolgozok[i].Nev} [{dolgozok[i].Reszleg}] - {dolgozok[i].Fizetes} Ft");
+                if (dolgozok.Count == 0) Console.WriteLine("Üres az adatbázis.");
+                Console.WriteLine("\n--- LISTA ---");
+                for (int i = 0; i < dolgozok.Count; i++){
+                Console.WriteLine($"{i + 1}. {dolgozok[i].Nev} [{dolgozok[i].Reszleg}] - {dolgozok[i].Fizetes} Ft");
+                }
+                Console.ReadKey();
+                }
+            static void Mentes(){
+                using (StreamWriter sw = new StreamWriter(fajlnev)){
+                    for (int i = 0; i < dolgozok.Count; i++){
+                        sw.WriteLine($"{dolgozok[i].Nev};{dolgozok[i].Reszleg};{dolgozok[i].Fizetes}");
+                    }
+                }
             }
-            Console.ReadKey();
-            }
-        }
+         }
     }
 }
 
